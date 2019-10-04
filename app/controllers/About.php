42347d60a519f93a1ccaaa6a7ea23c4dcefd9a35
Raden Ariffudin Rachman <radenariffudin@mail.unpas.ac.id>
2019-10-04 21:42:07 +0700

    Membuat view pada aplikasi MVC dan memisahkan header dan footer

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,13 +1,22 @@
 <?php
 
-class About {
+class About extends Controller {
 
     // method default
-    public function index($nama = "Raden Ariffudin Rachman", $pekerjaan = "Mahasiswa"){
-        echo "Halo, Nama Saya $nama, Saya Adalah Seorang $pekerjaan";
+    public function index($nama = "Raden Ariffudin Rachman", $pekerjaan = "Mahasiswa", $umur = 20){
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
 
     public function page(){
-        echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
