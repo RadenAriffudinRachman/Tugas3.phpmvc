f1ad03d7421176c803f18563b28f376fbf64aeaa
Raden Ariffudin Rachman <radenariffudin@mail.unpas.ac.id>
2019-10-04 23:37:24 +0700

    menambahkan assets dengan menggunakan bootstrap dan menambahkan navbar dan menggunakan jumbotron

---
+++ app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/Tugas3.phpmvc/public');
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
-    <h1>About Me</h1>
-    <p>Halo, Nama Saya <?= $data['nama']; ?>, Umur Saya <?= $data['umur']; ?> Tahun. Saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+<div class="container">
+    <h1 class="mt-3">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/ari.jpg" alt="Raden Ariffudin Rachman" width="250" height="250" class="rounded-circle shadow mt-3">
+    <p class="mt-3">Halo, Nama Saya <?= $data['nama']; ?>, Umur Saya <?= $data['umur']; ?> Tahun. Saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
