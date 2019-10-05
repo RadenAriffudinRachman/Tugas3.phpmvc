d85dcf5fb858a80859d85ff8a6490c5011c1b3c8
Raden Ariffudin Rachman <radenariffudin@mail.unpas.ac.id>
2019-10-06 02:39:08 +0700

    Database wrapper

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/Tugas3.phpmvc/public');
+
+// DB
+define ('DB_HOST','localhost');
+define ('DB_USER', 'root');
+define ('DB_PASS', '');
+define ('DB_NAME', 'Tugas3.phpmvc');
