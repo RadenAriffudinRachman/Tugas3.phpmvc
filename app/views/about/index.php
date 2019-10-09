d03118b638212158fa15d501976f3e67eb4f09ac
Raden Ariffudin Rachman <radenariffudin@mail.unpas.ac.id>
2019-10-09 15:27:53 +0700

    merubah about agar berada di tengah

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,5 +1,5 @@
-<div class="container">
+<div class="container text-center">
     <h1 class="mt-3">About Me</h1>
     <img src="<?= BASEURL; ?>/img/ari.jpg" alt="Raden Ariffudin Rachman" width="250" height="250" class="rounded-circle shadow mt-3">
-    <p class="mt-3">Halo, Nama Saya <?= $data['nama']; ?>, Umur Saya <?= $data['umur']; ?> Tahun. Saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+    <p class="mt-3">Halo, Nama Saya <?= $data['nama']; ?>, Umur Saya <?= $data['umur']; ?> Tahun. Saya adalah seorang <?= $data['pekerjaan']; ?>. Saya kuliah di Universitas Pasundan Bandung, dan saya mengambil Program Studi Teknik Informatika.</p>
 </div>
